5dcf11e845883f2bdbd68dbe94a71f9e04820d4f
mocviu <mailfordevelopmentandstuff@gmail.com>
2023-07-04 01:18:42 +0500

    shoot

---
+++ Game_1/stuff/objects/bullets/bullet.gd
@@ -0,0 +1,8 @@
+extends KinematicBody2D
+
+var velocity = Vector2.RIGHT
+var speed = 300
+var damage = 1
+
+func _physics_process(delta):
+	var collision_info = move_and_collide(velocity.normalized() * delta * speed)
--- Game_1/stuff/objects/player/player.gd
+++ Game_1/stuff/objects/player/player.gd
@@ -4,6 +4,8 @@ var vector = Vector2.ZERO
 var vertical_speed = 350.0
 var horizontal_speed = 350.0
 
+const bullet_path = preload('res://stuff/objects/bullets/bullet.tscn')
+
 func _physics_process(delta):
 	if Input.is_action_pressed("move_right"):
 		position.x += horizontal_speed * delta
@@ -13,3 +15,12 @@ func _physics_process(delta):
 		position.y -= vertical_speed * delta
 	if Input.is_action_pressed("move_down"):
 		position.y += vertical_speed * delta
+	if Input.is_action_pressed("ui_accept"):
+		shoot(0)
+
+
+func shoot(bullet_number):
+	var bullet = bullet_path.instance()
+	bullet.position = position
+	get_parent().add_child(bullet)
+	
